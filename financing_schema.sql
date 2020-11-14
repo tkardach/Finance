5850b3379349514c43d493e3b1b100afd15a2d8f
Kardach <thomas.kardach@thermofisher.com>
2020-11-13 16:43:08 -0800

    Update .sql file; add readme

--- financing_schema.sql
+++ financing_schema.sql
@@ -24,14 +24,14 @@ DROP TABLE IF EXISTS `account`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `account` (
   `account_id` int NOT NULL AUTO_INCREMENT,
-  `balance` decimal(15,2) NOT NULL DEFAULT '0.00',
   `name` varchar(45) NOT NULL,
   `profile` int NOT NULL,
-  PRIMARY KEY (`account_id`),
+  `balance` decimal(15,2) NOT NULL DEFAULT '0.00',
+  PRIMARY KEY (`name`,`profile`),
   UNIQUE KEY `unique_id` (`account_id`),
   KEY `FK_Profile_idx` (`profile`),
   CONSTRAINT `FK_Profile` FOREIGN KEY (`profile`) REFERENCES `profile` (`profile_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=73 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2573 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -48,7 +48,7 @@ CREATE TABLE `profile` (
   PRIMARY KEY (`profile_id`),
   UNIQUE KEY `unique_email` (`email`),
   UNIQUE KEY `unique_id` (`profile_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=547 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3449 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -59,20 +59,20 @@ DROP TABLE IF EXISTS `recurring_transaction`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `recurring_transaction` (
-  `recurring_id` int NOT NULL AUTO_INCREMENT,
+  `recurring_transaction_id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
+  `account` int NOT NULL,
   `start_date` datetime NOT NULL,
   `timespan` varchar(45) NOT NULL,
   `amount` decimal(15,2) NOT NULL DEFAULT '0.00',
-  `to_account` int NOT NULL,
-  `from_account` int NOT NULL,
-  PRIMARY KEY (`recurring_id`),
-  UNIQUE KEY `unique_id` (`recurring_id`),
-  KEY `FK_Account_idx` (`to_account`),
-  KEY `FK_FromAccount_idx` (`from_account`),
-  CONSTRAINT `FK_FromAccount` FOREIGN KEY (`from_account`) REFERENCES `account` (`account_id`),
-  CONSTRAINT `FK_ToAccount` FOREIGN KEY (`to_account`) REFERENCES `account` (`account_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `to_account` int DEFAULT NULL,
+  PRIMARY KEY (`recurring_transaction_id`),
+  UNIQUE KEY `transaction_id_UNIQUE` (`recurring_transaction_id`),
+  KEY `FK_FromAccount_idx` (`account`),
+  KEY `FK_ToAccount_recurring_idx` (`to_account`),
+  CONSTRAINT `FK_Account_recurring` FOREIGN KEY (`account`) REFERENCES `account` (`account_id`),
+  CONSTRAINT `FK_ToAccount_recurring` FOREIGN KEY (`to_account`) REFERENCES `account` (`account_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=88 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -83,19 +83,19 @@ DROP TABLE IF EXISTS `single_transaction`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `single_transaction` (
-  `single_id` int NOT NULL AUTO_INCREMENT,
+  `single_transaction_id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
+  `account` int NOT NULL,
   `date` datetime NOT NULL,
   `amount` decimal(15,2) NOT NULL DEFAULT '0.00',
-  `to_account` int NOT NULL,
-  `from_account` int NOT NULL,
-  PRIMARY KEY (`single_id`),
-  UNIQUE KEY `unique_id` (`single_id`),
-  KEY `FK_ToAccount_idx` (`to_account`),
-  KEY `FK_FromAccount_idx` (`from_account`),
-  CONSTRAINT `FK_FromAccount_single` FOREIGN KEY (`from_account`) REFERENCES `account` (`account_id`),
+  `to_account` int DEFAULT NULL,
+  PRIMARY KEY (`single_transaction_id`),
+  UNIQUE KEY `single_transaction_id_UNIQUE` (`single_transaction_id`),
+  KEY `FK_ToAccount_idx` (`account`),
+  KEY `FK_ToAccount_single_idx` (`to_account`),
+  CONSTRAINT `FK_Account_single` FOREIGN KEY (`account`) REFERENCES `account` (`account_id`),
   CONSTRAINT `FK_ToAccount_single` FOREIGN KEY (`to_account`) REFERENCES `account` (`account_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=419 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -107,4 +107,4 @@ CREATE TABLE `single_transaction` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-11-13  9:33:41
+-- Dump completed on 2020-11-13 16:42:39
